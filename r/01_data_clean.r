1b78ce891a8c1eeb8e485db15f637d23c4d81b1c
jas1 <jspairani.registros@gmail.com>
2019-04-05 01:36:39 -0300

    logre parsear los cups bien completos con fechas y todo
    Changes to be committed:
    modified:   r/01_data_clean.r
    new file:   r/export_partidos_all_parsed.txt
    
    logre parsear los cups bien completos con fechas y todo
    
    - falta traducir
    - falta mergear cup.txt con cup-finals.txt
    
    y luego exportar eso a un csv

--- r/01_data_clean.r
+++ r/01_data_clean.r
@@ -19,7 +19,7 @@ parse_copas_extraer_grupos <- function(datos_copa){
         tidyr::separate(value,into=c('grupo','paises'),sep='\\|') %>% 
         mutate(grupo=stringr::str_trim(grupo)) %>% 
         mutate(paises=stringr::str_trim(paises)) %>% 
-        tidyr::separate(paises,into=c("pais1","pais2","pais3","pais4")) %>% 
+        tidyr::separate(paises,into=c("pais1","pais2","pais3","pais4","pais5","pais6","pais7","pais8","pais9","pais10")) %>% 
         tidyr::gather(gr,pais,pais1,pais2,pais3,pais4) %>% 
         filter(!is.na(pais)) %>% 
         select(-gr,-grupo) %>% 
@@ -36,7 +36,7 @@ extraer_partidos <-  function(datos_copa){
     
     partidos
 }
-parse_partidos <- function(partidos_data,listado_indices){
+parse_partidos <- function(partidos_data,listado_indices,listado_fechas,anio){
     
     regex_equipos_fechas <- rebus::one_or_more(rebus::ALPHA) %R%
         rebus::optional(rebus::one_or_more(rebus::BLANK)) %R%
@@ -53,45 +53,19 @@ parse_partidos <- function(partidos_data,listado_indices){
     # partidos_data <- partidos_data[[1]]
     # listado_indices <- listado_indices_split[indice_para_fake]
     # listado_indices <- listado_indices[[1]]
-    # partidos_full_crudo <- data.frame(partidos,goles,stringsAsFactors = FALSE) %>% as_tibble() 
-    # partidos_full_crudo <- data.frame(partidos=partidos_data[[1]],stringsAsFactors = FALSE) %>% as_tibble()
     partidos_full_crudo <- data.frame(partidos=partidos_data,stringsAsFactors = FALSE) %>% as_tibble() 
     partidos_estadio_ciudad <- partidos_full_crudo %>% 
-        # parseo de partidos
         tidyr::separate(partidos,sep="@",into=c("partido","estadio")) %>% 
-        # locacion
         tidyr::separate(estadio,sep=",",into=c("estadio","ciudad")) %>% 
         mutate(estadio=str_trim(estadio)) %>% 
         mutate(ciudad=str_trim(ciudad)) 
     
     partidos_pos_fecha <- partidos_estadio_ciudad %>% 
-        #equipos
-        # select(partido) %>% 
         mutate(partido_orden=stringr::str_trim(stringr::str_sub(partido,listado_indices[1],listado_indices[2])))%>% 
-        mutate(fecha=stringr::str_trim(stringr::str_sub(partido,listado_indices[3],listado_indices[4])))
-        
-    # # Austria        3–2 a.e.t. (1-1, 1-1) France   
-    # regex_equipos_2 <- rebus::one_or_more(rebus::WRD) %R% rebus::optional(rebus::BLANK %R% rebus::one_or_more(rebus::WRD))
-    # 
-    
-    # t1 <- stringr::str_sub("(1) 27 May   Sweden         3–2 (1-1)  Argentina      ",start = listado_indices[4]+1)
-    # t2 <- stringr::str_sub("(17) 10 June   Italy 2–1 a.e.t. (1-1, 0-0)  Czechoslovakia  ",start = listado_indices[4]+1)
-    # t3 <- stringr::str_sub("(7) 27 May   Italy          7–1 (3-0)  United States  ",start = listado_indices[4]+1)
-    # str_replace_all(t1,rebus::DGT,"")
-    # str_replace_all("-","")
-    # 
-    # str_replace_all(t2,"a\\.e\\.t\\.","") %>% str_replace_all("[^[:alpha:][:space:]]","")
-    # 
-    # stringr::str_extract_all(str_replace_all(t1,"[^[:alpha:][:space:]]",""),regex_equipos_fechas)
-    # stringr::str_extract_all(str_replace_all(t2,"[^[:alpha:][:space:]]",""),regex_equipos_fechas)
-    # stringr::str_extract_all(str_replace_all(t3,"[^[:alpha:][:space:]]",""),regex_equipos_fechas)
-    # stringr::str_extract(t1,regex_puntos_parentesis)
-   # stringr::str_replace_all(t2,regex_parentesis,"") %>% stringr::str_extract_all(one_or_more(DGT))
-   # stringr::str_replace_all(t1,regex_parentesis,"") %>% stringr::str_extract_all(one_or_more(DGT))
-   # stringr::str_replace_all(t2,regex_parentesis,"")  %>% stringr::str_extract_all(one_or_more(DGT))
-   # stringr::str_replace_all(t3,regex_parentesis,"")  %>% stringr::str_extract_all(one_or_more(DGT))
-    
-    
+        mutate(fecha=stringr::str_trim(stringr::str_sub(partido,listado_indices[3],listado_indices[4]))) %>% 
+        mutate(fecha=paste0(fecha," ",anio)) %>% # YmdHMS
+        mutate(fecha=parse_date(fecha,format=listado_fechas))
+ 
     partidos_nombre_equipos <- partidos_pos_fecha %>% 
         mutate(equipos=stringr::str_trim(stringr::str_sub(partido,start = listado_indices[4]+1))) %>%
         mutate(equipos=str_replace_all(equipos,"a\\.e\\.t\\.","")) %>% 
@@ -108,7 +82,7 @@ parse_partidos <- function(partidos_data,listado_indices){
         mutate(resultado=stringr::str_replace_all(resultado,regex_parentesis,"")) %>% 
         mutate(resultado=stringr::str_extract_all(resultado,rebus::one_or_more(rebus::DGT))) %>% 
         mutate(resultado=purrr::map(resultado,.f=function(x){paste0(collapse=";",x)})) %>% tidyr::unnest(resultado) %>% #View()
-        tidyr::separate(resultado,sep=";",into=c("goles_eq_1","goles_eq_2")) %>% 
+        tidyr::separate(resultado,sep=";",into=c("goles_eq_1","goles_eq_2")) 
  
     partidos_filtro_final<- partidos_resultados %>% 
         select(-partido,partido_orden,fecha,nombre_eq_1,nombre_eq_2,goles_eq_1,goles_eq_2)
@@ -122,32 +96,69 @@ wc_data <- list.files(path = here::here(),pattern = wc_path_dirs_regex) %>%
     tibble::enframe() %>% 
     rename(dir_name = value) %>% 
     mutate(file_wc_data= purrr::map(dir_name,.f = function(x){
-        # read_cup_data <- here::here(x,"cup.txt")
-        # read_cup_data
-        "cup.txt"
+        file_name <- "cup.txt"
+        read_cup_data <- here::here(x,file_name)
+        retu <- file.exists(read_cup_data)
+        if (!retu) {
+            retu <- NA
+        }else{
+            retu <- file_name
+        }
+        retu
     }) ) %>% 
     tidyr::unnest(file_wc_data) %>%
+    mutate(file_wc_finals_data= purrr::map(dir_name,.f = function(x){
+        file_name <- "cup_finals.txt"
+        read_cup_data <- here::here(x,file_name)
+        retu <- file.exists(read_cup_data)
+        if (!retu) {
+            retu <- NA
+        }else{
+            retu <- file_name
+        }
+        retu
+    }) ) %>% 
+    tidyr::unnest(file_wc_finals_data) %>% 
     mutate(file_wc_teams_data = purrr::map(dir_name,.f = function(x){
         read_cup_data <- here::here(x,"squads")
-        list.files(read_cup_data)
+        retu <- list.files(read_cup_data)
+        if (length(retu) == 0) {
+            retu <- NA
+        }
+        retu
     }) ) %>% 
     tidyr::unnest(file_wc_teams_data)
+
+# length(list.files(here::here("2018--russia","squads"))) > 0
+# length(list.files(here::here("2014--brazil","squads"))) > 0
+# View(wc_data)
 
 # obtener los datos de los archivos ------------------------------------------------------
 
 wc_cups_data <- wc_data %>% 
-    count(dir_name,file_wc_data,name = 'dato_cant_equipos') %>% 
+    nest(-dir_name,-file_wc_data,-file_wc_finals_data,.key = archivos_equipos) %>% # no funciona porque no tengo todos los equipos aca.
     mutate(current_path =  here::here(dir_name,file_wc_data)) %>% 
     mutate(wc_cup_texto = purrr::map(current_path,readr::read_lines)) %>% 
-    select(-file_wc_data,-current_path)
-
+    mutate(wc_cup_finals_texto = purrr::map( .x=here::here(dir_name,file_wc_finals_data),
+                                             .f=function(x){
+                                                 
+                                                 retu <- NA
+                                                 if (!stringr::str_ends(x,"NA")) {
+                                                     retu <- readr::read_lines(x)   
+                                                 }
+                                                 retu
+        
+    })) %>% 
+    select(-file_wc_data,-current_path) %>% 
+    tidyr::separate(dir_name,into = c('anio','sponsor'),sep='--')
+View(wc_cups_data)
 # wc_parsed_cups_data %>% tidyr::unnest(wc_cup_texto)
 
 wc_teams_by_cup_data <- wc_data %>% 
     mutate(team_path =  here::here(dir_name,'squads',file_wc_teams_data)) %>% 
     mutate(wc_cup_team_texto = purrr::map(team_path,readr::read_lines)) %>% 
     select(-file_wc_data,-team_path) %>% 
-    tidyr::separate(dir_name,into = c('year','location'),sep='--')
+    tidyr::separate(dir_name,into = c('anio','sponsor'),sep='--')
 # wc_teams_by_cup_data %>% tidyr::unnest(wc_cup_team_texto)
 
 # parsear torneo -------------------------------------------------------
@@ -162,100 +173,42 @@ listado_indices_split <- list('1930'=c(1,4,5,13),'1934'=c(1,4,5,13),'1938'=c(1,4
                               '2010'=c(1,4,5,23),
                               '2014'=c(1,4,5,23))
 
-wc_cups_data <- wc_cups_data %>% tidyr::separate(dir_name,into = c('year','location'),sep='--')
+listado_parse_fecha <- list('1930'='%d %B %Y','1934'='%d %B %Y','1938'='%d %B %Y','1950'='%d %B %Y','1954'='%d %B %Y','1958'='%d %B %Y','1962'='%d %B %Y','1966'='%d %B %Y','1970'='%d %B %Y','1974'='%d %B %Y','1978'='%d %B %Y','1982'='%d %B %Y','1986'='%d %B %Y','1990'='%d %B %Y','1994'='%d %B %Y','1998'='%d %B %Y','2002'='%d %B %Y',
+                              '2006'="%a %b/%d %Y",#Wed Jun/21 2006
+                              '2010'="%a %b/%d %H:%M %Y",#Thu Jun/24 16:00 2010
+                              '2014'="%a %b/%d %H:%M %Y")#Thu Jun/12 17:00 2014
+# paste0("'",wc_cups_data$anio[1:17],"'='%d %B %Y'",collapse = ",")
 # wc_cups_data[1,]$wc_cup_texto
-wc_cups_data <- wc_cups_data %>% 
+wc_cups_data_processed_1 <- wc_cups_data[1,] %>% 
     mutate(grupos=purrr::map(.x=wc_cup_texto,.f=parse_copas_extraer_grupos)) %>% 
     mutate(partidos_crudo=purrr::map(.x=wc_cup_texto,.f=extraer_partidos)) %>% 
     mutate(partidos_parsed=purrr::pmap(.l=list(partidos_data=partidos_crudo,
-                                              listado_indices=listado_indices_split),
-                                       .f=parse_partidos))# %>%
+                                              listado_indices=listado_indices_split[1],
+                                              listado_fechas=listado_parse_fecha,
+                                              anio=anio),
+                                       .f=parse_partidos))
+
+wc_cups_data_processed <- wc_cups_data %>% 
+    mutate(grupos=purrr::map(.x=wc_cup_texto,.f=parse_copas_extraer_grupos)) %>% 
+    mutate(partidos_crudo=purrr::map(.x=wc_cup_texto,.f=extraer_partidos)) %>% 
+    mutate(partidos_parsed=purrr::pmap(.l=list(partidos_data=partidos_crudo,
+                                               listado_indices=listado_indices_split,
+                                               listado_fechas=listado_parse_fecha,
+                                               anio=anio),
+                                       .f=parse_partidos))
+# %>%
 # 
     # NULL
 
+current_idx <- 20
+wc_cups_data_processed$partidos_parsed[current_idx]
+# wc_cups_data_processed$partidos_parsed[current_idx][[1]] %>% View()
+wc_cups_data_processed$partidos_crudo[current_idx]
 
-wc_cups_data$partidos_parsed[2]
-wc_cups_data$partidos_crudo[2]
+
+# creo que con tener ya todos los equipos resultados  es mas que suficiente para esta edicion
+# que esten las fechas bien, traducir y listo
+# me falt ahacer el merge de los que tienen " final" , a que este todo en un " partidos " y exportar eso.
+# si todo va bien, podria reciclar lo que hice con cup.txt
 
 # parsear equipos ---------------------------------------------------------
-# aca hay que armar un parser de equipos y un parser de copas.
-
-ej_datos_copa1 <- wc_cups_data[1,]$wc_cup_texto
-ej_datos_copa2 <- wc_cups_data[20,]$wc_cup_texto
-
-
-# generador de indices que son iguales
-paste0("'",wc_cups_data$year[1:17],"'=c(1,4,5,13)",collapse = ",")
-# tail(wc_cups_data$year)
-
-
-
-
-
-#1 hasta index "blank" +1
-#desde index blank+1 +1 +
-
-texto <- wc_cups_data[3,]$wc_cup_texto
-
-stringr::str_starts(string=texto,pattern = "#")
-
-wc_cups_data$partidos_crudo[3]
-
-wc_cups_data %>% select(partidos_crudo) %>% unnest() %>% pull() %>% 
-    write_lines(x = .,path=here::here("r","export_partidos_all.txt"))
-
-
-wc_cups_data$partidos_crudo
-current_idx <- 1
-partidos_data <- wc_cups_data$partidos_crudo[[current_idx]]
-listado_indices <- listado_indices_split[[current_idx]]
-
-
-resutlado_1 <- parser_copas(ej_datos_copa1)
-resutlado_1$
-# $partidos %>% View()
-parser_copas(ej_datos_copa2)
-    
-
-texto <- ej_datos_copa1[[1]]
-nombre_copa <- texto[[2]]
-
-
-
-grupos_regex <- "Group" %R% one_or_more(rebus::BLANK) %R% one_or_more(rebus::ALNUM)%R% one_or_more(rebus::BLANK)%R% rebus::PIPE
-grupos <- texto[texto %>% stringr::str_detect(grupos_regex)]
-
-grs <- data.frame(grupo_linea=c(''))
-grs <- grs[-1,]
-mchs <- NA # aca datos match + goles
-
-# es grupo
-grupos_regex <- "Group" %R% one_or_more(rebus::BLANK) %R% one_or_more(rebus::ALNUM)%R% one_or_more(rebus::BLANK)%R% rebus::PIPE
-grupo_match_regex <- "Group" %R% one_or_more(rebus::BLANK) %R% one_or_more(rebus::ALNUM)%R% "\\:"
-# es match
-# si seccion = "Group 1:"  , es match
-# es gol
-# ""
-# linea_ant <- NA
-# for(linea_idx in 1:length(texto)){
-#     
-#     if ( texto[linea_idx] %>% str_detect(grupos_regex)) {
-#         grs <- grs %>% dplyr::add_row(grs)
-#     }
-#     
-#     if ( texto[linea_idx] %>% str_detect(grupo_match_regex)) {
-#         grs <- grs %>% dplyr::add_row(grs)
-#     }
-#     
-#     linea_ant <- texto[linea_idx]
-# }
-
-
-# para version JULIO ------------------------------------------------------
-
-# "TORNEO: " > para identificar linea nombre torneo
-
-# "GRUPO: " > para identificar linea GRUPOS
-# "FECHA: " > para identificar linea FECHAS PARTIDOS > esta sirve para despues cruzar con los matchs
-# "MATCH: " > para identificar linea MATCHS / PARTIDOS >esta ahora incluye los goles
-# hay que lograr parsear eso , si funciona traduzco el resto de los mundilaes a mano.
