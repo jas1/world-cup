721708fa90dedbb465f5ff2826608ba659cc1018
jas1 <jspairani.registros@gmail.com>
2019-04-06 02:28:25 -0300

    version final para revisar por otros de los archivos
     Changes to be committed:
    	modified:   r/01_data_clean.r
    	new file:   r/data/20190406_diccionario_solo_equipos.txt
    	new file:   r/data/20190406_diccionario_solo_equipos_input.txt
    	new file:   r/data/20190406_en_final_matches.txt
    	new file:   r/data/20190406_es_final_partidos.txt
    	deleted:    r/export_partidos_all.txt
    	deleted:    r/export_partidos_all_parsed.txt
    
    script con idiomas.
    fix de nombres de 3 palabras.
    diccionario de equipos de entrada ( traducido )
    diccionario de equipos escrito para traducir. ( el de entrada es el traducido )
    saque datos viejos de partidos y parseos.

--- r/01_data_clean.r
+++ r/01_data_clean.r
@@ -46,14 +46,16 @@ extraer_partidos <-  function(datos_copa){
 parse_finales <- function(partidos_data,listado_indices,listado_fechas,anio){
     retu <- NA
     if (length(partidos_data) > 1) {
-        retu <-parse_partidos(partidos_data,listado_indices,listado_fechas,anio)    
+        retu <-parse_partidos(partidos_data,listado_indices,listado_fechas,anio,es_finales = TRUE)    
     }
     retu
 }
-parse_partidos <- function(partidos_data,listado_indices,listado_fechas,anio){
+parse_partidos <- function(partidos_data,listado_indices,listado_fechas,anio,es_finales=FALSE){
     
     regex_equipos_fechas <- rebus::one_or_more(rebus::ALPHA) %R%
         rebus::optional(rebus::one_or_more(rebus::BLANK)) %R%
+        rebus::one_or_more(rebus::WRD) %R%
+        rebus::optional(rebus::one_or_more(rebus::BLANK)) %R% # 3 palabras
         rebus::one_or_more(rebus::WRD)
     
     regex_puntos <- rebus::one_or_more(rebus::DGT) %R% '-' %R% rebus::one_or_more(rebus::DGT)
@@ -62,11 +64,6 @@ parse_partidos <- function(partidos_data,listado_indices,listado_fechas,anio){
         rebus::one_or_more(rebus::negated_char_class(rebus::ALPHA)) %R% 
         rebus::CLOSE_PAREN
 
-    # indice_para_fake <- 2    
-    # partidos_data <- wc_cups_data$partidos_crudo[indice_para_fake]
-    # partidos_data <- partidos_data[[1]]
-    # listado_indices <- listado_indices_split[indice_para_fake]
-    # listado_indices <- listado_indices[[1]]
     partidos_full_crudo <- data.frame(partidos=partidos_data,stringsAsFactors = FALSE) %>% as_tibble() 
     partidos_estadio_ciudad <- partidos_full_crudo %>% 
         tidyr::separate(partidos,sep="@",into=c("partido","estadio")) %>% 
@@ -80,34 +77,7 @@ parse_partidos <- function(partidos_data,listado_indices,listado_fechas,anio){
         mutate(fecha=paste0(fecha," ",anio)) %>% # YmdHMS
         mutate(fecha=parse_date(fecha,format=listado_fechas))
  
-    # df <- data.frame(textos=c(ex1,ex2,ex3,ex4,ex5,ex6,ex7),stringsAsFactors = FALSE) %>% as_tibble()
-    # ex1<- "(55) Tue Jun/29 16:00   Paraguay       0-0 0-0nV 5-3iE  Japan         @ Loftus Versfeld Stadium, Pretoria"
-    # ex2<- "(58) Fri Jul/2 20:30   Uruguay        1-1 1-1nV 4-2iE   Ghana         @ Soccer City, Johannesburg"
-    # ex3<- "(64) Sun Jul/11 20:30  Netherlands  0-0 0-1nV  Spain     @ Soccer City, Johannesburg"
-    # ex4<- "(64) Sun Jul/13 16:00  Germany 1-0 a.e.t. (0-0, 0-0) Argentina     @ Estádio do Maracanã, Rio de Janeiro (UTC-3)   # W61 - W62"
-    # ex5<- "(62) Wed Jul/9 17:00  Netherlands  2-4 pen. 0-0 a.e.t. (0-0, 0-0)  Argentina        @ Arena de São Paulo, São Paulo (UTC-3)    # W59 - W69  "
-    # ex6<- "(45) 21 June    Brazil  3-4pen 1-1aet 1-1  France          @ Estadio Jalisco, Guadalajara"
-    # ex7<- "(38) 15 June   Soviet Union  3-4 a.e.t.  2-2   Belgium  @ Estadio Nou Camp, León"
-    # 
-    # 
-    # df %>% 
-    #     tidyr::separate(textos,into=c("partido","estadio"),sep="@") %>% 
-    #     mutate(equipos=stringr::str_trim(stringr::str_sub(partido,start = 13))) %>% #13 o 22
-    #     mutate(equipos= stringr::str_replace_all(equipos,"iE","")) %>% 
-    #     mutate(equipos= stringr::str_replace_all(equipos,"nV","")) %>% 
-    #     # mutate(equipos= stringr::str_replace_all(equipos,"aet","")) %>% 
-    #     mutate(equipos= stringr::str_replace_all(equipos,"a"%R%rebus::optional(rebus::DOT)%R%"e"%R%rebus::optional(rebus::DOT)%R%"t"%R%rebus::optional(rebus::DOT),"")) %>% 
-    #     mutate(equipos= stringr::str_replace_all(equipos,"pen"%R%rebus::optional(rebus::DOT),"")) %>% 
-    #     mutate(equipos=str_replace_all(equipos,"[^[:alpha:][:space:]]","")) %>% 
-    #     
-    #     mutate(equipos=stringr::str_extract_all(equipos,regex_equipos_fechas)) %>% 
-    #     mutate(equipos=purrr::map(equipos,.f=function(x){paste0(collapse=";",x)})) %>% tidyr::unnest(equipos) %>% #View()
-    #     tidyr::separate(equipos,sep=";",into=c("nombre_eq_1","nombre_eq_2")) %>% 
-    #     mutate(nombre_eq_1=stringr::str_trim(nombre_eq_1)) %>% #pull(equipos)
-    #     mutate(nombre_eq_2=stringr::str_trim(nombre_eq_2)) %>% 
-    #     select(nombre_eq_1,nombre_eq_2)
-        # mutate(equipos= stringr::str_replace_all(equipos,"pen","")) %>% 
-
+    
     
     # si contiene iE , si contiene nV, es de un tipo de partidos si tiene iE, es sacar 
     # " 5-3iE" creo que es penales
@@ -133,14 +103,47 @@ parse_partidos <- function(partidos_data,listado_indices,listado_fechas,anio){
         mutate(nombre_eq_1=stringr::str_trim(nombre_eq_1)) %>% #pull(equipos)
         mutate(nombre_eq_2=stringr::str_trim(nombre_eq_2)) #pull(equipos)
     
-    partidos_resultados <- partidos_nombre_equipos %>% 
-        mutate(resultado=stringr::str_trim(stringr::str_sub(partido,start = listado_indices[4]+1))) %>%
-        mutate(resultado=stringr::str_replace_all(resultado,regex_parentesis,"")) %>% 
-        mutate(resultado=stringr::str_extract_all(resultado,rebus::one_or_more(rebus::DGT))) %>% 
-        mutate(resultado=purrr::map(resultado,.f=function(x){paste0(collapse=";",x)})) %>% tidyr::unnest(resultado) %>% #View()
-        tidyr::separate(resultado,sep=";",into=c("goles_eq_1","goles_eq_2")) 
- 
-    partidos_filtro_final<- partidos_resultados %>% 
+    partidos_resultados <- partidos_nombre_equipos 
+    
+    # if (es_finales) {
+        partidos_resultados <- partidos_nombre_equipos %>% 
+            mutate(resultado=stringr::str_trim(stringr::str_sub(partido,start = listado_indices[4]+1))) %>%
+            mutate(resultado=stringr::str_extract_all(resultado,regex_puntos)) %>%
+            mutate(resultado=purrr::map(resultado,function(x){
+                # print()
+                retu <- NA
+                for (resu_actual in x) {
+                    splited <- str_split(resu_actual,"-") %>% unlist()
+                    splited1 <- splited[[1]]
+                    splited2 <- splited[[2]]
+
+                    retu <- resu_actual
+                    if (splited1 != splited2) {
+                        return(resu_actual)
+                    }
+
+                }
+                retu # si no devolvio el diferente devuelve el valor,
+                # esto para rescatar los empates cuando no es final
+            })) %>%
+            unnest(resultado) %>% 
+            separate(resultado,into=c("goles_eq_1","goles_eq_2"))
+    # }else{
+
+        # partidos_resultados <- partidos_nombre_equipos %>%
+        #     mutate(resultado=stringr::str_trim(stringr::str_sub(partido,start = listado_indices[4]+1))) %>%
+        #     mutate(resultado=stringr::str_replace_all(resultado,regex_parentesis,"")) %>%
+        #     mutate(resultado=stringr::str_extract_all(resultado,rebus::one_or_more(rebus::DGT))) %>%
+        #     # mutate(resultado=purrr::map(resultado,.f=function(x){paste0(collapse=";",x)})) %>% tidyr::unnest(resultado) %>% #View()
+        #     mutate(resultado_tmp=purrr::map(resultado,.f=function(x){paste0(collapse=";",x)})) %>% tidyr::unnest(resultado_tmp) %>% #View()
+        #     mutate(resultado2=purrr::map(resultado,.f=function(x){paste0(collapse=";",x)})) %>% #View()
+        #     mutate(mas_2_result=purrr::map(resultado2,.f=function(x){length(str_split(x,pattern = ";"))})) %>% #View()
+        #     tidyr::separate(resultado_tmp,sep=";",into=c("goles_eq_1","goles_eq_2"))
+
+    # }
+    
+    # partidos_filtro_final<- partidos_resultados #%>%
+    partidos_filtro_final<- partidos_resultados %>%
         select(-partido,partido_orden,fecha,nombre_eq_1,nombre_eq_2,goles_eq_1,goles_eq_2)
     
     partidos_filtro_final
@@ -234,8 +237,7 @@ listado_parse_fecha <- list('1930'='%d %B %Y','1934'='%d %B %Y','1938'='%d %B %Y
                               '2010'="%a %b/%d %H:%M %Y",#Thu Jun/24 16:00 2010
                               '2014'="%a %b/%d %H:%M %Y",
                               '2018'="%a %b/%d %H:%M %Y")#Thu Jun/12 17:00 2014
-# paste0("'",wc_cups_data$anio[1:17],"'='%d %B %Y'",collapse = ",")
-# wc_cups_data[1,]$wc_cup_texto
+
 wc_cups_data_processed_1 <- wc_cups_data[1,] %>% 
     mutate(grupos=purrr::map(.x=wc_cup_texto,.f=parse_copas_extraer_grupos)) %>% 
     mutate(partidos_crudo=purrr::map(.x=wc_cup_texto,.f=extraer_partidos)) %>% 
@@ -255,11 +257,6 @@ wc_cups_data_processed <- wc_cups_data %>%
                                        .f=parse_partidos))
 
 
-current_idx <- 21
-wc_cups_data_processed$partidos_parsed[current_idx]
-# wc_cups_data_processed$partidos_parsed[current_idx][[1]] %>% View()
-wc_cups_data_processed$partidos_crudo[current_idx]
-
 
 # creo que con tener ya todos los equipos resultados  es mas que suficiente para esta 1ra edicion
 # que esten las fechas bien, traducir y listo
@@ -275,19 +272,70 @@ wc_cups_data_processed_con_finales <- wc_cups_data_processed %>%
                                    .f=parse_finales))
     
 
-current_idx_2 <- 21
-wc_cups_data_processed_con_finales$finales_parsed[current_idx_2]
-# wc_cups_data_processed$partidos_parsed[current_idx][[1]] %>% View()
-wc_cups_data_processed_con_finales$finales_crudo[current_idx_2]
 
-# wc_cups_data_processed_con_finales %>% filter(!is.na(file_wc_finals_data)) %>% unnest(finales_parsed) %>% 
-#     # count(anio,fecha) %>% View()
-#     filter(is.na(fecha))
+# dataset final para ingles -----------------------------------------------
 
-wc_cups_data_processed_con_finales %>% filter(!is.na(file_wc_finals_data)) %>% unnest(finales_parsed) %>% View()
+df_para_union_1 <- wc_cups_data_processed %>% unnest(partidos_parsed)
+# los partidos sacados de los archivos de finales. no todos los mundiales tienen las finales por separado.
+df_para_union_2 <- wc_cups_data_processed_con_finales %>% filter(!is.na(file_wc_finals_data)) %>%  unnest(finales_parsed)
 
-glimpse(wc_cups_data_processed_con_finales)
+todos_partidos <- df_para_union_1 %>% dplyr::union_all(df_para_union_2)
 
+version_final <- todos_partidos %>% 
+    mutate(sponsor=stringr::str_replace_all(sponsor,"-"," ")) %>%
+    mutate(sponsor=stringr::str_to_sentence(sponsor)) %>% 
+    mutate(sponsor=if_else(sponsor=="South korea n japan","South Korea and Japan",sponsor)) %>% 
+    # count(sponsor )%>% View()
+    select(anio,sponsor,estadio,ciudad,partido_orden,fecha,nombre_eq_1,nombre_eq_2,goles_eq_1,goles_eq_2)
+
+version_final_en <- version_final %>% 
+    rename(year=anio,host=sponsor,stadium=estadio,city=ciudad,match_order=partido_orden,
+           date=fecha,team_1=nombre_eq_1,team_2=nombre_eq_2,team_1_score=goles_eq_1,team_2_score=goles_eq_2)
+
+# readr::write_delim(version_final_en,here::here("r","data","20190406_en_final_matches.txt"),delim = "\t")
+
+
+# traduccion_español ------------------------------------------------------
+
+version_es <- version_final %>% 
+    rename(anfitrion=sponsor,equipo_1=nombre_eq_1,equipo_2=nombre_eq_2,equipo_1_final=goles_eq_1,equipo_2_final=goles_eq_2)
+
+# tmp_equipos <- version_es %>% count(equipo_1) %>% rename(eq=equipo_1)
+# tmp_equipos_2 <- version_es %>% count(equipo_2)%>% rename(eq=equipo_2)
+# diccionario_solo_equipos <- tmp_equipos %>% dplyr::union(tmp_equipos_2) %>% select(eq) %>% count(eq) %>% select(eq) %>% 
+#     mutate(pais_es="")
+
+# readr::write_csv(diccionario_solo_equipos,here::here("r","data","20190406_2_diccionario_solo_equipos.txt"))
+# bug en nombres de algunos paises.Emirates United Arab	Arabes unidos ; Trinidad and ; Republic of
+# tmp_crudo <- wc_cups_data_processed %>% select(partidos_crudo) %>% unnest()
+# tmp_crudo_finales <- wc_cups_data_processed_con_finales %>% select(partidos_crudo) %>% unnest()
+# partidos_crudos <- tmp_crudo %>% dplyr::union_all(tmp_crudo_finales) 
+# 
+# partidos_crudos %>% filter(str_detect(partidos_crudo,"United Arab"))
+# partidos_crudos %>% filter(str_detect(partidos_crudo,"Trinidad and"))
+# partidos_crudos %>% filter(str_detect(partidos_crudo,"Republic of"))
+# partidos_crudos %>% filter(str_detect(partidos_crudo,"Dutch East Indies"))
+
+# Cargar paises para traducir
+diccionario_paises <- readr::read_delim(here::here("r","data","20190406_diccionario_solo_equipos_input.txt"),delim = "\t")
+diccionario_paises <- diccionario_paises %>% mutate(eq_2=str_to_upper(eq))
+version_es %>% filter(str_detect(equipo_2,"Germany"))
+
+version_es_final <- version_es %>% 
+    left_join(diccionario_paises, by=c("equipo_1"="eq")) %>% 
+    mutate(equipo_1=pais_es) %>% 
+    select(-pais_es,-eq_2) %>% 
+    left_join(diccionario_paises, by=c("equipo_2"="eq")) %>% 
+    mutate(equipo_2=pais_es) %>% 
+    select(-pais_es,-eq_2) %>% 
+    mutate(anfitrion_2=str_to_upper(anfitrion)) %>% 
+    left_join(diccionario_paises, by=c("anfitrion_2"="eq_2")) %>% 
+    # mutate(anfitrion=if_else(anfitrion=="South Korea and Japan","Corea del sur y Japón",anfitrion))
+    mutate(anfitrion=if_else(anfitrion=="South Korea and Japan","Corea del sur y Japón",pais_es)) %>% 
+    select(-pais_es,-eq,-anfitrion_2)
+
+# readr::write_delim(version_es_final,here::here("r","data","20190406_es_final_partidos.txt"),delim = "\t")
+    
 # parsear equipos ---------------------------------------------------------
 wc_teams_by_cup_data <- wc_data %>% 
     mutate(team_path =  here::here(dir_name,'squads',file_wc_teams_data)) %>% 
@@ -301,3 +349,95 @@ wc_teams_by_cup_data <- wc_data %>%
                                                retu
                                            })) %>% 
     select(-file_wc_data,-team_path)
+
+
+# comentarios para borrar en algun momento --------------------------------
+
+
+# ex1<- "(55) Tue Jun/29 16:00   Paraguay       0-0 0-0nV 5-3iE  Japan         @ Loftus Versfeld Stadium, Pretoria"
+# ex2<- "(58) Fri Jul/2 20:30   Uruguay        1-1 1-1nV 4-2iE   Ghana         @ Soccer City, Johannesburg"
+# ex3<- "(64) Sun Jul/11 20:30  Netherlands  0-0 0-1nV  Spain     @ Soccer City, Johannesburg"
+# ex4<- "(64) Sun Jul/13 16:00  Germany 1-0 a.e.t. (0-0, 0-0) Argentina     @ Estádio do Maracanã, Rio de Janeiro (UTC-3)   # W61 - W62"
+# ex5<- "(62) Wed Jul/9 17:00  Netherlands  2-4 pen. 0-0 a.e.t. (0-0, 0-0)  Argentina        @ Arena de São Paulo, São Paulo (UTC-3)    # W59 - W69  "
+# ex6<- "(45) 21 June    Brazil  3-4pen 1-1aet 1-1  France          @ Estadio Jalisco, Guadalajara"
+# ex7<- "(38) 15 June   Soviet Union  4-4 a.e.t.  1-2   Belgium  @ Estadio Nou Camp, León"
+# df <- data.frame(textos=c(ex1,ex2,ex3,ex4,ex5,ex6,ex7),stringsAsFactors = FALSE) %>% as_tibble()
+
+# df %>%
+#     tidyr::separate(textos,into=c("partido","estadio"),sep="@") %>%
+#     select(partido) %>% 
+#     mutate(puntos=stringr::str_extract_all(partido,regex_puntos)) %>%
+#     mutate(puntos=purrr::map(puntos,function(x){
+#         # print()
+#         for (resu_actual in x) {
+#             splited <- str_split(resu_actual,"-") %>% unlist()
+#             print(splited)
+#             splited1 <- splited[[1]]
+#             splited2 <- splited[[2]]
+#             
+#             print(splited2)
+#             print(splited1)
+#             
+#             retu <- NA
+#             if (splited1 != splited2) {
+#                 return(resu_actual)
+#             }
+#         }
+#     })) %>%
+#     unnest(puntos) %>% 
+#     separate(puntos,into=c("goles_eq_1","goles_eq_2"))
+# unnest(puntos)
+# proceso puntos finales
+
+
+# proceso nombres
+#     mutate(equipos=stringr::str_trim(stringr::str_sub(partido,start = 13))) %>% #13 o 22
+#     mutate(equipos= stringr::str_replace_all(equipos,"iE","")) %>% 
+#     mutate(equipos= stringr::str_replace_all(equipos,"nV","")) %>% 
+#     # mutate(equipos= stringr::str_replace_all(equipos,"aet","")) %>% 
+#     mutate(equipos= stringr::str_replace_all(equipos,"a"%R%rebus::optional(rebus::DOT)%R%"e"%R%rebus::optional(rebus::DOT)%R%"t"%R%rebus::optional(rebus::DOT),"")) %>% 
+#     mutate(equipos= stringr::str_replace_all(equipos,"pen"%R%rebus::optional(rebus::DOT),"")) %>% 
+#     mutate(equipos=str_replace_all(equipos,"[^[:alpha:][:space:]]","")) %>% 
+#     
+#     mutate(equipos=stringr::str_extract_all(equipos,regex_equipos_fechas)) %>% 
+#     mutate(equipos=purrr::map(equipos,.f=function(x){paste0(collapse=";",x)})) %>% tidyr::unnest(equipos) %>% #View()
+#     tidyr::separate(equipos,sep=";",into=c("nombre_eq_1","nombre_eq_2")) %>% 
+#     mutate(nombre_eq_1=stringr::str_trim(nombre_eq_1)) %>% #pull(equipos)
+#     mutate(nombre_eq_2=stringr::str_trim(nombre_eq_2)) %>% 
+#     select(nombre_eq_1,nombre_eq_2)
+# mutate(equipos= stringr::str_replace_all(equipos,"pen","")) %>% 
+
+
+
+
+# current_idx <- 21
+# wc_cups_data_processed$partidos_parsed[current_idx]
+# # wc_cups_data_processed$partidos_parsed[current_idx][[1]] %>% View()
+# wc_cups_data_processed$partidos_crudo[current_idx]
+# 
+# wc_cups_data_processed %>% unnest(partidos_parsed) %>%  unnest(mas_2_result ) %>% count(mas_2_result)
+# wc_cups_data_processed %>% unnest(partidos_parsed) %>%  unnest(resultado2 ) %>% select(resultado2)  %>%  unnest(mas_2_result ) 
+#     filter(mas_2_result)
+#     
+#     wc_cups_data_processed %>% unnest(partidos_parsed) %>%  unnest(resultado2 ) %>% slice(358) %>% pull(partido)
+
+
+
+
+# current_idx_2 <- 21
+# wc_cups_data_processed_con_finales$finales_parsed[current_idx_2]
+# # wc_cups_data_processed_con_finales$partidos_parsed[current_idx][[1]] %>% View()
+# wc_cups_data_processed_con_finales$finales_crudo[current_idx_2]
+
+# wc_cups_data_processed_con_finales %>% 
+#     filter(!is.na(file_wc_finals_data)) %>% 
+#     unnest(finales_parsed) %>% 
+#     filter(goles_eq_1==goles_eq_2)
+
+# wc_cups_data_processed_con_finales %>% filter(!is.na(file_wc_finals_data)) %>% unnest(finales_parsed) %>% 
+#     # count(anio,fecha) %>% View()
+#     filter(is.na(fecha))
+
+# wc_cups_data_processed_con_finales %>% filter(!is.na(file_wc_finals_data)) %>% unnest(finales_parsed) %>% View()
+
+# glimpse(wc_cups_data_processed_con_finales)
